a19dd1e9fc27dd0dc11626219960458cc220ea4e
IsaquiasMarques <patisaquias2000@gmail.com>
2022-10-30 14:24:00 +0100

    first steps for the website

---
+++ assets/tailwind.conf/tailwindcss.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        screens: {
+          'mobile': '320px',
+          'laptop': '1024px',
+          'desktop': '1536px'
+        }
+      }
+    }
+}
